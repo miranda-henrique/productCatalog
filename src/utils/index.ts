8e46c21d49d36719779db85d14270d50880bd557
Henrique Miranda <miranda.henrique@outlook.com.br>
2022-04-17 16:29:09 -0300

    Initial setup and navbar

---
+++ src/utils/index.ts
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router';
+
+
+export function returnSelect(route: string) {
+    const router = useRouter();
+    const { pathname } = router;
+
+    let result;
+    if (pathname === route) {
+        result = 'active';
+    }
+
+    return result;
+}
