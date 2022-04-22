7174af71ab8253e9d02e86406445f5ae27be5219
Henrique Miranda <miranda.henrique@outlook.com.br>
2022-04-22 19:59:51 -0300

    Change bootstrap import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { Navbar } from '../components';
 
+import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/custom.scss';
 import '../styles/globals.css';
 
@@ -11,12 +12,6 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-          crossOrigin="anonymous"
-        />
-
         <title>DSCatalog NextJS</title>
       </Head>
 
